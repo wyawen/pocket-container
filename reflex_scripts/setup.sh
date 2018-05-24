9df4261850ec78b46fe676a409cabd18dd78916d
Debian <admin@ip-10-1-172-181.us-west-2.compute.internal>
2018-05-24 07:46:58 +0000

    Fix reflex reset scrip: now allow re-running setup.sh on the same instance

--- reflex_scripts/setup.sh
+++ reflex_scripts/setup.sh
@@ -2,10 +2,14 @@
 
 # update ReFlex ip
 if ifconfig eth1 ; then
-	ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | sed -i "47s@.*@$(awk '{print "crail.storage.reflex.bindip             "$1""}')@g" ../crail/conf/crail-site.conf
-	ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | sed -i "15s@.*@$(awk '{print "host_addr=\""$1"/16\""}')@g" ix.conf
-	ifconfig eth1 down
-fi 
+        ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' >> /dev/reflex_ip
+        ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | sed -i "47s@.*@$(awk '{print "crail.storage.reflex.bindip             "$1""}')@g" ../crail/conf/crail-site.conf
+        ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | sed -i "15s@.*@$(awk '{print "host_addr=\""$1"/16\""}')@g" ix.conf
+        ifconfig eth1 down
+else
+        cat /dev/reflex_ip | sed -i "47s@.*@$(awk '{print "crail.storage.reflex.bindip             "$1""}')@g" ../crail/conf/crail-site.conf
+        cat /dev/reflex_ip | sed -i "15s@.*@$(awk '{print "host_addr=\""$1"/16\""}')@g" ix.conf
+fi
 
 modprobe uio
 insmod deps/dpdk/build/kmod/igb_uio.ko
